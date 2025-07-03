dd6e85bf8909dfc3bb46dcb70362e1ff7b74a4ce
dao <letrongdao98@gmail.com>
2025-07-03 09:56:40 +0700

    Update axios.js

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: import.meta.env.VITE_SERVER_URL,
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
